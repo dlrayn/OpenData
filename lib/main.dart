9b7db3595636121652bacf903fb959cced6f27ac
delll11206 <delll11206@gmail.com>
2024-08-08 05:07:16 +0000

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false, // Menghilangkan label debug
       home: TabScreen(),
     );
   }
